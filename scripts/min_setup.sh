929a29e189ff239daaa30f5fcdf12e672b66f31f
Conner Brooks <connerbrooks@gmail.com>
2019-02-12 02:07:22 -0800

    Cleanup.

--- scripts/min_setup.sh
+++ scripts/min_setup.sh
@@ -1,8 +1,14 @@
 #!/bin/sh
 # sets up nvidia drivers and my core tools
 
+echo  "Performing minimal xubuntu setup"
 sudo ubuntu-drivers autoinstall
 sudo apt-get install zsh vim git curl conky compton
 
-chsh -s $(which zsh)
+if [ $SHELL != $(which zsh) ]; then
+    echo 'Changing shell...'
+    chsh -s $(which zsh)
+else
+    echo "Zsh is already the default shell."
+fi
 
